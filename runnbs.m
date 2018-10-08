eb5d58e25a1da662016b480135d6b1730948b2fa
Srivas Chennu <srivas@gmail.com>
2018-10-09 00:00:32 +0100

    Checked in Tue Oct  9 00:00:32 BST 2018

--- runnbs.m
+++ runnbs.m
@@ -16,13 +16,18 @@ rng('default');
 load(sprintf('%s/groupdata_%s.mat',filepath,listname));
 
 covariates = {
-    0   'crsdiag'
-    1   'crsr'
-    0   'age'
-    0   'tbi'
-    0   'subjnum'
-    0   'days_onset'
-};
+    1   'age'
+    0   'gender'
+    };
+
+% covariates = {
+%     0   'crsdiag'
+%     1   'crsr'
+%     0   'age'
+%     0   'tbi'
+%     0   'subjnum'
+%     0   'days_onset'
+% };
 
 design_matrix = subjlist.(covariates{1,2});
 for c = 2:size(covariates,1)
@@ -33,8 +38,8 @@ trange = 0.9:-0.1:0.1;
 testdata = squeeze(getfeatures(listname,measure,bandidx,'trange',trange));
 
 %select only patients
-testdata = testdata(subjlist.crsdiag < 4,:);
-design_matrix = design_matrix(subjlist.crsdiag < 4,:);
+% testdata = testdata(subjlist.crsdiag < 4,:);
+% design_matrix = design_matrix(subjlist.crsdiag < 4,:);
 
 fprintf('Estimating GLM with %d permutations and %d covariates on %d subjects...\n', ...
     nperm, size(covariates,1), size(testdata,1));
